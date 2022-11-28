e540d9d6095ff0907fcd321dfc1d816860629453
YoshimasaIwano <44yoshimasa03@gmail.com>
2022-11-28 02:15:06 -0800

    implemented hiragara classifier to depploy

--- app/server.py
+++ app/server.py
@@ -1,8 +1,8 @@
 '''
-mnister (keras save_model) by YoshimasaIwano, Kaiyu0128
+Hiragana Classifier by YoshimasaIwano, Kaiyu0128
 '''
-import argparse
-import tensorflow
+# import argparse
+import tensorflow as tf
 import base64
 import json
 import numpy as np
@@ -14,37 +14,31 @@ from io import BytesIO
 from PIL import Image
 from waitress import serve
 
+from model import EfficientHiragana
+
 
 app = Flask(__name__)
 
-# validation
-def validation():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-c', '--colab',
-                        action="store_true",
-                        help='Specify when running on Google Colab.(use flask-ngrok)')
-    args = parser.parse_args()
-    return args
-
-#  check argument
-args = validation()
-if args.colab:    
-    print("use flask-ngrok.")
-    run_with_ngrok(app)
-
-
-
-
 # labels
-label = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 class_names = ['a', 'e', 'ha', 'he', 'hi', 'ho', 'hu', 'i', 'ka', 'ke', 'ki', 'ko', 'ku', 'ma', 'me', 'mi', 'mo', 'mu', 'na', 'ne', 'ni', 'nn', 'no', 'nu', 'o', 'ra', 're', 'ri', 'ro', 'ru', 'sa', 'se', 'si', 'so', 'su', 'ta', 'te', 'ti', 'to', 'tu', 'u', 'wa', 'wo', 'ya', 'yo', 'yu']
 
 # loading model and summary
-model = load_model('colab_mnist.hdf5')
+IMG_SIZE = 48
+input_shape = IMG_SIZE 
+output_shape = 46
+model = EfficientHiragana(input_shape, output_shape)
+optimizer = tf.keras.optimizers.Adam()
+model.build(input_shape=(None, IMG_SIZE, IMG_SIZE, 3))
+model.compile(
+    optimizer=optimizer,
+    loss='categorical_crossentropy',
+    metrics=['accuracy']
+)
+model.load_weights('EfficientNetB0_Hiragana.h5')
 model.summary()
 
 # Input Size classification
-_, img_w, img_h, img_ch = model.layers[0].input.shape
+img_w, img_h, img_ch = IMG_SIZE, IMG_SIZE, 3
 print("img_w:{} img_h:{} img_ch:{}".format(img_w, img_h, img_ch))
 
 # index.html
@@ -61,22 +55,22 @@ def output():
     b64_pngdata = request.json['b64_pngdata']
 
     # base64 decode
-    tmpdata = b64_pngdata.split(',')  
-    bindata = base64.b64decode(tmpdata[1])  
+    tmp = b64_pngdata.split(',')  
+    bin_data = base64.b64decode(tmp[1])  
 
     # Resizing input image size
-    img_PIL = Image.open(BytesIO(bindata)).convert('RGB')
+    img = Image.open(BytesIO(bin_data)).convert('RGB')
     if img_ch == 1:
-        img_PIL = img_PIL.convert('L')     
-    img_PIL = img_PIL.resize((img_w, img_h))    
+        img = img.convert('L')     
+    img = img.resize((img_w, img_h))    
 
-    # pillow→binary
+    # pillow to binary
     with BytesIO() as output_png:
-        img_PIL.save(output_png, format="PNG")
-        contents = output_png.getvalue()  
+        img.save(output_png, format="PNG")
+        img_values = output_png.getvalue()  
 
-    # mnist type convert
-    x = img_to_array(img_PIL) / 255
+    # standarlization
+    x = img_to_array(img) 
     x = x[None, ...]
 
     # prediction
@@ -85,7 +79,7 @@ def output():
     print(pred)
 
     # predicted label
-    pred_label = label[int(np.argmax(pred[0]))]
+    pred_label = class_names[int(np.argmax(pred[0]))]
     print("label:", pred_label)
 
     # predicted score
@@ -94,30 +88,17 @@ def output():
     print("------------------------------------------------------------------")
 
     # base64 encode
-    tmp_data = str(base64.b64encode(contents))
-    tmp_data = tmp_data[2:-1]  
+    data = str(base64.b64encode(img_values))
+    data = data[2:-1]  
 
-    data1 = "data:image/png;base64," + tmp_data
-    data2 = pred_label
-    data3 = score
-    label_score = [str("{:.10f}".format(n)) for n in pred[0]]
+    data = "data:image/png;base64," + data
 
-    return_data = {"pred_png": data1,
-                   "pred_label": data2,
-                   "pred_score": data3,
-                   "label0": label_score[0],
-                   "label1": label_score[1],
-                   "label2": label_score[2],
-                   "label3": label_score[3],
-                   "label4": label_score[4],
-                   "label5": label_score[5],
-                   "label6": label_score[6],
-                   "label7": label_score[7],
-                   "label8": label_score[8],
-                   "label9": label_score[9]
+    return_data = {"pred_png": data,
+                   "pred_label": pred_label,
+                   "pred_score": score,
                    }
 
-    return jsonify(ResultSet=json.dumps(return_data))
+    return jsonify(results=json.dumps(return_data))
 
 # For debug
 def display(data, name):
