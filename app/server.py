8dc707f3efdaf0c069e37106c33dfd46f81f2728
YoshimasaIwano <44yoshimasa03@gmail.com>
2022-11-28 12:32:21 -0800

    able to deploy hiragana classifier

--- app/server.py
+++ app/server.py
@@ -8,7 +8,7 @@ import json
 import numpy as np
 from flask import Flask, request, render_template, jsonify
 # from flask_ngrok import run_with_ngrok
-# from tensorflow.keras.models import load_model
+from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.image import img_to_array
 from io import BytesIO
 from PIL import Image
@@ -17,10 +17,102 @@ from waitress import serve
 from model import EfficientHiragana
 
 
+# app = Flask(__name__)
+
+# # labels
+# class_names = ['a', 'e', 'ha', 'he', 'hi', 'ho', 'hu', 'i', 'ka', 'ke', 'ki', 'ko', 'ku', 'ma', 'me', 'mi', 'mo', 'mu', 'na', 'ne', 'ni', 'nn', 'no', 'nu', 'o', 'ra', 're', 'ri', 'ro', 'ru', 'sa', 'se', 'si', 'so', 'su', 'ta', 'te', 'ti', 'to', 'tu', 'u', 'wa', 'wo', 'ya', 'yo', 'yu']
+
+# # loading model and summary
+# IMG_SIZE = 48
+# input_shape = IMG_SIZE 
+# output_shape = 46
+# model = EfficientHiragana(input_shape, output_shape)
+# optimizer = tf.keras.optimizers.Adam()
+# model.build(input_shape=(None, IMG_SIZE, IMG_SIZE, 3))
+# model.compile(
+#     optimizer=optimizer,
+#     loss='categorical_crossentropy',
+#     metrics=['accuracy']
+# )
+# model.load_weights('EfficientNetB0_Hiragana.h5')
+# model.summary()
+
+# # Input Size classification
+# img_w, img_h, img_ch = IMG_SIZE, IMG_SIZE, 3
+# print("img_w:{} img_h:{} img_ch:{}".format(img_w, img_h, img_ch))
+
+# # index.html
+# @app.route("/")
+# def index():
+#     return render_template('index.html')
+
+
+
+# # Activate when hiragana is posted from input.js
+# @app.route('/output', methods=['POST'])
+# def output():
+#     # Receiving hiragana input as json type
+#     b64_pngdata = request.json['b64_pngdata']
+
+#     # base64 decode
+#     tmp = b64_pngdata.split(',')  
+#     bin_data = base64.b64decode(tmp[1])  
+
+#     # Resizing input image size
+#     img = Image.open(BytesIO(bin_data)).convert('RGB')
+#     if img_ch == 1:
+#         img = img.convert('L')     
+#     img = img.resize((img_w, img_h))    
+
+#     # pillow to binary
+#     with BytesIO() as output_png:
+#         img.save(output_png, format="PNG")
+#         img_values = output_png.getvalue()  
+
+#     # standarlization
+#     x = img_to_array(img) 
+#     x = x[None, ...]
+
+#     # prediction
+#     pred = model.predict(x)
+#     np.set_printoptions(suppress=True, precision=10, floatmode='fixed')
+#     print(pred)
+
+#     # predicted label
+#     pred_label = class_names[int(np.argmax(pred[0]))]
+#     print("label:", pred_label)
+
+#     # predicted score
+#     score = str("{:.10f}".format(np.max(pred)))
+#     print("score:", score)
+#     print("------------------------------------------------------------------")
+
+#     # base64 encode
+#     data = str(base64.b64encode(img_values))
+#     data = data[2:-1]  
+
+#     img_data = "data:image/png;base64," + data
+
+#     return_data = {"pred_png": img_data,
+#                    "pred_label": pred_label,
+#                    "pred_score": score,
+#                    }
+
+#     return jsonify(results=json.dumps(return_data))
+
 app = Flask(__name__)
 
 # labels
+label = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 class_names = ['a', 'e', 'ha', 'he', 'hi', 'ho', 'hu', 'i', 'ka', 'ke', 'ki', 'ko', 'ku', 'ma', 'me', 'mi', 'mo', 'mu', 'na', 'ne', 'ni', 'nn', 'no', 'nu', 'o', 'ra', 're', 'ri', 'ro', 'ru', 'sa', 'se', 'si', 'so', 'su', 'ta', 'te', 'ti', 'to', 'tu', 'u', 'wa', 'wo', 'ya', 'yo', 'yu']
+
+# # loading model and summary
+# model = load_model('colab_mnist.hdf5')
+# model.summary()
+
+# # Input Size classification
+# _, img_w, img_h, img_ch = model.layers[0].input.shape
+# print("img_w:{} img_h:{} img_ch:{}".format(img_w, img_h, img_ch))
 
 # loading model and summary
 IMG_SIZE = 48
@@ -55,22 +147,22 @@ def output():
     b64_pngdata = request.json['b64_pngdata']
 
     # base64 decode
-    tmp = b64_pngdata.split(',')  
-    bin_data = base64.b64decode(tmp[1])  
+    tmpdata = b64_pngdata.split(',')  
+    bindata = base64.b64decode(tmpdata[1])  
 
     # Resizing input image size
-    img = Image.open(BytesIO(bin_data)).convert('RGB')
+    img_PIL = Image.open(BytesIO(bindata)).convert('RGB')
     if img_ch == 1:
-        img = img.convert('L')     
-    img = img.resize((img_w, img_h))    
+        img_PIL = img_PIL.convert('L')     
+    img_PIL = img_PIL.resize((img_w, img_h))    
 
-    # pillow to binary
+    # pillow→binary
     with BytesIO() as output_png:
-        img.save(output_png, format="PNG")
-        img_values = output_png.getvalue()  
+        img_PIL.save(output_png, format="PNG")
+        contents = output_png.getvalue()  
 
-    # standarlization
-    x = img_to_array(img) 
+    # mnist type convert
+    x = img_to_array(img_PIL) #/ 255
     x = x[None, ...]
 
     # prediction
@@ -88,17 +180,20 @@ def output():
     print("------------------------------------------------------------------")
 
     # base64 encode
-    data = str(base64.b64encode(img_values))
-    data = data[2:-1]  
+    tmp_data = str(base64.b64encode(contents))
+    tmp_data = tmp_data[2:-1]  
 
-    img_data = "data:image/png;base64," + data
+    data1 = "data:image/png;base64," + tmp_data
+    data2 = pred_label
+    data3 = score
+    # label_score = [str("{:.10f}".format(n)) for n in pred[0]]
 
-    return_data = {"pred_png": img_data,
-                   "pred_label": pred_label,
-                   "pred_score": score,
+    return_data = {"pred_png": data1,
+                   "pred_label": data2,
+                   "pred_score": data3,
                    }
 
-    return jsonify(results=json.dumps(return_data))
+    return jsonify(ResultSet=json.dumps(return_data))
 
 # For debug
 def display(data, name):
--- app/static/Input.js
+++ app/static/Input.js
@@ -1,175 +1,377 @@
+// // use Createjs and JQuery
+// window.addEventListener("load", init);
+// function init() {
+//     // canvas object
+//     let drawCanvas = new createjs.Stage("WriteCanvas");
+
+//     // enable touch Event if possible
+//     if (createjs.Touch.isSupported()) {
+//         createjs.Touch.enable(drawCanvas);
+//     }
+
+//     let shape = new createjs.Shape();
+//     drawCanvas.addChild(shape);             
+//     resetEvent();
+
+//     // set mousedown event listener
+//     drawCanvas.addEventListener("stagemousedown", handleDown);
+
+//     // when mouse is down
+//     function handleDown(event) {
+//         //  black: "#000000", white: "#FFFFFF"
+//         let paintColor = "#000000"
+
+//         // start to draw
+//         shape.graphics
+//             .beginStroke(paintColor)
+//             .setStrokeStyle(20, "round") 
+//             .moveTo(event.stageX, event.stageY);
+
+//         // add events mouse move and up
+//         drawCanvas.addEventListener("stagemousemove", handleMove);
+//         drawCanvas.addEventListener("stagemouseup", handleUp);
+//     }
+
+//     // mouse move events
+//     function handleMove(event) {
+//         shape.graphics.lineTo(event.stageX, event.stageY);
+//     }
+
+//     // mouse up events
+//     function handleUp(event) {
+//         shape.graphics.lineTo(event.stageX, event.stageY);
+//         shape.graphics.endStroke();
+
+//         // release the move and up events
+//         drawCanvas.removeEventListener("stagemousemove", handleMove);
+//         drawCanvas.removeEventListener("stagemouseup", handleUp);
+//     }
+
+//     // To update image
+//     createjs.Ticker.timingMode = createjs.Ticker.RAF;
+//     createjs.Ticker.addEventListener("tick", onTick);
+
+//     function onTick() {
+//         drawCanvas.update();
+//     }
+
+
+//     // Buttuon Object
+//     let buttons = new createjs.Stage("ButtonCanvas");
+//     buttons.enableMouseOver();
+
+//     // create predict button
+//     let pred_button = createButton("Predict", 80, 30, "#0650c7");
+//     pred_button.x = 20;
+//     pred_button.y = 10;
+//     buttons.addChild(pred_button);
+
+//     let reset_button = createButton("Reset", 80, 30, "#ff6161");
+//     reset_button.x = 110;
+//     reset_button.y = 10;
+//     buttons.addChild(reset_button);
+
+//     // add click events
+//     pred_button.addEventListener("click", predictEvent);
+//     reset_button.addEventListener("click", resetEvent);
+
+//     // predict button event 
+//     function predictEvent(event) {
+
+//         // trasform canvas to image
+//         drawCanvas.update();
+//         let png = drawCanvas.canvas.toDataURL();
+//         // document.getElementById("ChgPngImg").src = png;
+
+//         // JQuery
+//         // send data from javascript to python
+//         let textData = JSON.stringify({"b64_pngdata":png});
+//         $.ajax({
+//             type:'POST',
+//             url:'/output',
+//             data:textData,
+//             contentType:'application/json',
+
+//             success:function(json_data){
+//                 console.log(json_data);
+//                 let ret = JSON.parse(json_data.results);
+//                 document.getElementById("ResultImg").src = ret.pred_png;
+//                 document.getElementById("ResultLabel").textContent = ret.pred_label;
+//                 document.getElementById("ResultScore").textContent = ret.pred_score;
+//             }
+//         });
+//     }
+
+//     // reset button event
+//     function resetEvent(event) {
+//         shape.graphics.clear();
+//         shape.graphics.beginFill("white"); 
+//         shape.graphics.drawRect(0, 0, 240, 240);
+//         shape.graphics.endFill();
+//         drawCanvas.update();
+//         // let png = canvas.canvas.toDataURL();
+//         // document.getElementById("ChgPngImg").src = png;
+//     }
+
+//     // tick event
+//     createjs.Ticker.addEventListener("tick", handleTick);
+//     function handleTick() {
+//         buttons.update();
+//     }
+
+
+//     function createButton(name, width, height, keyColor) {
+//         // create button container
+//         let buttonContainer = new createjs.Container();
+//         buttonContainer.name = name; 
+//         buttonContainer.cursor = "pointer"; 
+
+//         // background of button
+//         let bgUp = new createjs.Shape();
+//         bgUp.graphics
+//             .setStrokeStyle(1.0)
+//             .beginStroke(keyColor)
+//             .beginFill("white")
+//             .drawRoundRect(0.5, 0.5, width - 1.0, height - 1.0, 4);
+//         buttonContainer.addChild(bgUp);
+//         bgUp.visible = true; 
+
+//         // mouse is hover 
+//         let bgOver = new createjs.Shape();
+//         bgOver.graphics
+//               .beginFill(keyColor)
+//               .drawRoundRect(0, 0, width, height, 4);
+//         bgOver.visible = false; 
+//         buttonContainer.addChild(bgOver);
+
+//         // label
+//         let label = new createjs.Text(name, "18px sans-serif", keyColor);
+//         label.x = width / 2;
+//         label.y = height / 2;
+//         label.textAlign = "center";
+//         label.textBaseline = "middle";
+//         buttonContainer.addChild(label);
+
+//         // add mouse events
+//         buttonContainer.addEventListener("mouseover", handleMouseOver);
+//         buttonContainer.addEventListener("mouseout", handleMouseOut);
+
+//         // when mouse is hover
+//         function handleMouseOver(event) {
+//             bgUp.visble = false;
+//             bgOver.visible = true;
+//             label.color = "white";
+//         }
+
+//         // mouse is out
+//         function handleMouseOut(event) {
+//             bgUp.visble = true;
+//             bgOver.visible = false;
+//             label.color = keyColor;
+//         }
+
+//         return buttonContainer;
+//     }
+// }
+
 // use Createjs and JQuery
 window.addEventListener("load", init);
 function init() {
-    // canvas object
-    let drawCanvas = new createjs.Stage("WriteCanvas");
 
-    // enable touch Event if possible
+    // --------------------------------------------------------------
+    // Stage1オブジェクト：WriteCanvas
+    // --------------------------------------------------------------
+    let stage1 = new createjs.Stage("WriteCanvas");
+
+    // タッチイベントが有効なブラウザの場合、
+    // CreateJSでタッチイベントを扱えるようにする
     if (createjs.Touch.isSupported()) {
-        createjs.Touch.enable(drawCanvas);
+        createjs.Touch.enable(stage1);
     }
 
-    let shape = new createjs.Shape();
-    drawCanvas.addChild(shape);             
-    resetEvent();
+    let shape = new createjs.Shape();   // シェイプを作成
+    stage1.addChild(shape);             // ステージに配置
 
-    // set mousedown event listener
-    drawCanvas.addEventListener("stagemousedown", handleDown);
+    handleClick_reset();
 
-    // when mouse is down
+    // ステージ上でマウスボタンを押した時のイベント設定
+    stage1.addEventListener("stagemousedown", handleDown);
+
+    // マウスを押した時に実行される
     function handleDown(event) {
-        //  black: "#000000", white: "#FFFFFF"
-        let paintColor = "#000000"
 
-        // start to draw
+        let paintColor = "#000000"                      // 筆ペンの色 black: "#000000", white: "#FFFFFF"
+
+        // 線の描画を開始
         shape.graphics
-            .beginStroke(paintColor)
-            .setStrokeStyle(20, "round") 
-            .moveTo(event.stageX, event.stageY);
+                .beginStroke(paintColor)                // 指定のカラーで描画
+                .setStrokeStyle(20, "round")            // 線の太さ、形
+                .moveTo(event.stageX, event.stageY);    // 描画開始位置を指定
 
-        // add events mouse move and up
-        drawCanvas.addEventListener("stagemousemove", handleMove);
-        drawCanvas.addEventListener("stagemouseup", handleUp);
+        // ステージ上でマウスを動かした時と離した時のイベント設定
+        stage1.addEventListener("stagemousemove", handleMove);
+        stage1.addEventListener("stagemouseup", handleUp);
     }
 
-    // mouse move events
+    // マウスが動いた時に実行する
     function handleMove(event) {
+
+        // マウス座標への線を引く
         shape.graphics.lineTo(event.stageX, event.stageY);
     }
 
-    // mouse up events
+    // マウスボタンが離された時に実行される
     function handleUp(event) {
+
+        // マウス座標への線を引く
         shape.graphics.lineTo(event.stageX, event.stageY);
+
+        // 線の描画を終了する
         shape.graphics.endStroke();
 
-        // release the move and up events
-        drawCanvas.removeEventListener("stagemousemove", handleMove);
-        drawCanvas.removeEventListener("stagemouseup", handleUp);
+        // イベント解除
+        stage1.removeEventListener("stagemousemove", handleMove);
+        stage1.removeEventListener("stagemouseup", handleUp);
     }
 
-    // To update image
     createjs.Ticker.timingMode = createjs.Ticker.RAF;
     createjs.Ticker.addEventListener("tick", onTick);
 
     function onTick() {
-        drawCanvas.update();
+        stage1.update(); // Stageの描画を更新
     }
 
+    // --------------------------------------------------------------
+    // Stage2オブジェクト：ButtonCanvas
+    // --------------------------------------------------------------
+    let stage2 = new createjs.Stage("ButtonCanvas");
+    stage2.enableMouseOver();
 
-    // Buttuon Object
-    let buttons = new createjs.Stage("ButtonCanvas");
-    buttons.enableMouseOver();
+    // ボタンを作成
+    let btn1 = createButton("Predict", 80, 30, "#0650c7");
+    btn1.x = 20;
+    btn1.y = 10;
+    stage2.addChild(btn1);
 
-    // create predict button
-    let pred_button = createButton("Predict", 80, 30, "#0650c7");
-    pred_button.x = 20;
-    pred_button.y = 10;
-    buttons.addChild(pred_button);
+    let btn2 = createButton("Reset", 80, 30, "#ff6161");
+    btn2.x = 110;
+    btn2.y = 10;
+    stage2.addChild(btn2);
 
-    let reset_button = createButton("Reset", 80, 30, "#ff6161");
-    reset_button.x = 110;
-    reset_button.y = 10;
-    buttons.addChild(reset_button);
+    // イベントを登録
+    btn1.addEventListener("click", handleClick_png);
+    btn2.addEventListener("click", handleClick_reset);
 
-    // add click events
-    pred_button.addEventListener("click", predictEvent);
-    reset_button.addEventListener("click", resetEvent);
+    // Predictボタン押下イベント
+    function handleClick_png(event) {
 
-    // predict button event 
-    function predictEvent(event) {
+        // Canvasタグから画像に変換
+        stage1.update();
+        let png = stage1.canvas.toDataURL();
+        document.getElementById("ChgPngImg").src = png;
 
-        // trasform canvas to image
-        drawCanvas.update();
-        let png = drawCanvas.canvas.toDataURL();
-        // document.getElementById("ChgPngImg").src = png;
-
-        // JQuery
-        // send data from javascript to python
+        // JQueryによるPOST処理
+        // javascript→pythonへPNGデータ転送
         let textData = JSON.stringify({"b64_pngdata":png});
+//      console.log(textData);
         $.ajax({
             type:'POST',
             url:'/output',
             data:textData,
             contentType:'application/json',
 
-            success:function(json_data){
-                console.log(json_data);
-                let ret = JSON.parse(json_data.results);
-                document.getElementById("ResultImg").src = ret.pred_png;
-                document.getElementById("ResultLabel").textContent = ret.pred_label;
-                document.getElementById("ResultScore").textContent = ret.pred_score;
+            // python→javascriptへデータ返送
+            // 非同期通信が成功したら実行される
+            success:function(data){
+                // 返却jsonデータからparseしてデータ取り出し
+//              console.log(data);
+                let result = JSON.parse(data.ResultSet);
+                document.getElementById("ResultImg").src = result.pred_png;
+                document.getElementById("ResultLabel").textContent = result.pred_label;
+                document.getElementById("ResultScore").textContent = result.pred_score;
             }
         });
     }
 
-    // reset button event
-    function resetEvent(event) {
+    // Restボタン押下イベント
+    function handleClick_reset(event) {
+
+        // シェイプのグラフィックスを消去
         shape.graphics.clear();
-        shape.graphics.beginFill("white"); 
+        shape.graphics.beginFill("white"); // background color
         shape.graphics.drawRect(0, 0, 240, 240);
         shape.graphics.endFill();
-        drawCanvas.update();
-        // let png = canvas.canvas.toDataURL();
-        // document.getElementById("ChgPngImg").src = png;
+        stage1.update();
+        let png = stage1.canvas.toDataURL();
+        document.getElementById("ChgPngImg").src = png;
     }
 
-    // tick event
+    // 時間経過イベント
     createjs.Ticker.addEventListener("tick", handleTick);
     function handleTick() {
-        buttons.update();
+
+        // Stage2の描画を更新
+        stage2.update();
     }
 
+    /**
+    * @param {String} text ボタンのラベル文言です。
+    * @param {Number} width ボタンの横幅(単位はpx)です。
+    * @param {Number} height ボタンの高さ(単位はpx)です。
+    * @param {String} keyColor ボタンのキーカラーです。
+    * @returns {createjs.Container} ボタンの参照を返します。
+    */
+    function createButton(text, width, height, keyColor) {
 
-    function createButton(name, width, height, keyColor) {
-        // create button container
-        let buttonContainer = new createjs.Container();
-        buttonContainer.name = name; 
-        buttonContainer.cursor = "pointer"; 
+        // ボタン要素をグループ化
+        let button = new createjs.Container();
+        button.name = text; // ボタンに参考までに名称を入れておく(必須ではない)
+        button.cursor = "pointer"; // ホバー時にカーソルを変更する
 
-        // background of button
+        // 通常時の座布団を作成
         let bgUp = new createjs.Shape();
         bgUp.graphics
-            .setStrokeStyle(1.0)
-            .beginStroke(keyColor)
-            .beginFill("white")
-            .drawRoundRect(0.5, 0.5, width - 1.0, height - 1.0, 4);
-        buttonContainer.addChild(bgUp);
-        bgUp.visible = true; 
+              .setStrokeStyle(1.0)
+              .beginStroke(keyColor)
+              .beginFill("white")
+              .drawRoundRect(0.5, 0.5, width - 1.0, height - 1.0, 4);
+        button.addChild(bgUp);
+        bgUp.visible = true; // 表示する
 
-        // mouse is hover 
+        // ロールオーバー時の座布団を作成
         let bgOver = new createjs.Shape();
         bgOver.graphics
               .beginFill(keyColor)
               .drawRoundRect(0, 0, width, height, 4);
-        bgOver.visible = false; 
-        buttonContainer.addChild(bgOver);
+        bgOver.visible = false; // 非表示にする
+        button.addChild(bgOver);
 
-        // label
-        let label = new createjs.Text(name, "18px sans-serif", keyColor);
+        // ラベルを作成
+        let label = new createjs.Text(text, "18px sans-serif", keyColor);
         label.x = width / 2;
         label.y = height / 2;
         label.textAlign = "center";
         label.textBaseline = "middle";
-        buttonContainer.addChild(label);
+        button.addChild(label);
 
-        // add mouse events
-        buttonContainer.addEventListener("mouseover", handleMouseOver);
-        buttonContainer.addEventListener("mouseout", handleMouseOut);
+        // ロールオーバーイベントを登録
+        button.addEventListener("mouseover", handleMouseOver);
+        button.addEventListener("mouseout", handleMouseOut);
 
-        // when mouse is hover
+        // マウスオーバイベント
         function handleMouseOver(event) {
             bgUp.visble = false;
             bgOver.visible = true;
             label.color = "white";
         }
 
-        // mouse is out
+        // マウスアウトイベント
         function handleMouseOut(event) {
             bgUp.visble = true;
             bgOver.visible = false;
             label.color = keyColor;
         }
 
-        return buttonContainer;
+        return button;
     }
 }
